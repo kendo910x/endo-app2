848b5718a2abd568ad5f9ceaae6c20cf5906914c
kendo910x <k.endo.spr@gmail.com>
2021-02-19 13:38:08 +0900

    added now time.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #coding: utf-8
 from flask import Flask, request, make_response
+import datetime
 app = Flask(__name__, static_folder='.')
 @app.route('/')
 def index():
-    html = '<html><body>endo app<br>Good-bye World!!</body></html>'
+    dt_now = datetime.datetime.now()
+    html = '<html><body>endo app<br>Good-bye World!!<br>' + dt_now.isoformat() + '</body></html>'
     return(html)
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', port=8081) 
